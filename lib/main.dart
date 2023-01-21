903b405b1a66e8b7dafa9efeff978e770b94c2f4
DavideIsaia <davide.enjoy@hotmail.it>
2023-01-21 16:34:19 +0100

    impostato provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import 'package:app_shop/providers/products.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.amber,
+    // solo il widget col notifier provider viene ricostruito quando qualcosa cambia nei dati, e non l'intera app
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(), // create se versione provider > 3, altrimenti builder
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.amber,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName:(context) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
