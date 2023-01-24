65e1305aa2f835ffae66886e4e8516bb68c53a1a
DavideIsaia <davide.enjoy@hotmail.it>
2023-01-24 16:32:25 +0100

    ordini recuperabili dal db

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -25,6 +25,34 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    const url =
+        'https://david-shop-default-rtdb.europe-west1.firebasedatabase.app/orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+        id: orderId,
+        amount: orderData['amount'],
+        dateTime: DateTime.parse(orderData['dateTime']),
+        products: (orderData['products'] as List<dynamic>)
+            .map((item) => CartItem(
+                  id: item['id'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                  title: item['title'],
+                ))
+            .toList(),
+      ));
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     const url =
         'https://david-shop-default-rtdb.europe-west1.firebasedatabase.app/orders.json';
@@ -38,7 +66,7 @@ class Orders with ChangeNotifier {
                     'id': cp.id,
                     'title': cp.title,
                     'quantity': cp.quantity,
-                    'price': cp.title,
+                    'price': cp.price,
                   })
               .toList(),
         }));
